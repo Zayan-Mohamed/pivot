1e132f73870353bbb4e29556b40c6be2ecca04f3
Zayan Mohamed <itsm.zayan@gmail.com>
2025-01-02 21:45:20 +0530

    Ignore build artifacts

--- server/dist/src/routes/ProjectRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const ProjectController_1 = require("../controllers/ProjectController");
-const router = (0, express_1.Router)();
-router.get("/", ProjectController_1.getProjects);
-router.post("/", ProjectController_1.createProject);
-exports.default = router;
